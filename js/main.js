3b857edc727b31551a76d844991e7134051338f6
Jan Hořava <czechinthemail@gmail.com>
2022-12-26 14:57:58 -0800

    cleared selected null files

--- js/main.js
+++ js/main.js
@@ -5,18 +5,22 @@ function repo_init(){
       'events': {
         'level-load-file': {
           'onclick': function(){
-              const files = document.getElementById('level-file').files;
-              if(files.length === 0){
+              const element = document.getElementById('level-file');
+              if(element.files.length === 0){
                   return;
               }
               core_file({
-                'file': files[0],
+                'file': element.files[0],
                 'todo': function(event){
-                    webgl_level_load({
-                      'character': -1,
-                      'json': JSON.parse(event.target.result),
-                    });
-                    document.title = webgl_properties['title'] || core_repo_title;
+                    if(webgl_level_load({
+                        'character': -1,
+                        'json': JSON.parse(event.target.result),
+                      })){
+                        document.title = webgl_properties['title'] || core_repo_title;
+
+                    }else{
+                        element.value = null;
+                    }
                 },
                 'type': 'readAsText',
               });
