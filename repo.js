085434e789d3741921ab7083be2c25f8bb71e4bb
Jan Hořava <czechinthemail@gmail.com>
2025-06-17 22:21:05 -0700

    changed core-menu to core_menu

--- repo.js
+++ repo.js
@@ -78,7 +78,7 @@ function repo_init(){
           'content': '<input id=level-file type=file><button id=level-load-file type=button>Load Level from File</button><br>'
             + '<button id=level-load-textarea type=button>Load Level from Textarea</button><br><textarea id=level-textarea></textarea>',
           'default': true,
-          'group': 'core-menu',
+          'group': 'core_menu',
           'label': 'Load Levels',
         },
       },
