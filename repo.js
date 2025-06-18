f26e311d744aaf4149f7f561df58a4571366029f
Jan Hořava <czechinthemail@gmail.com>
2025-06-18 10:45:07 -0700

    changed menu-lock to menu_lock

--- repo.js
+++ repo.js
@@ -55,7 +55,7 @@ function repo_init(){
           },
         },
       },
-      'menu-lock': true,
+      'menu_lock': true,
       'pointerbinds': {
         'contextmenu': {
           'preventDefault': true,
